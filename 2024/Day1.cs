7fcd891304b10e15f00c2afa409c1a9a533de70e
Michael Verban <verbanmike@gmail.com>
2024-11-30 23:32:55 -0600

    Day1 Part2

--- 2024/Day1.cs
+++ 2024/Day1.cs
@@ -8,7 +8,7 @@ namespace _2024
 {
 	public class Day1
 	{
-		public static int Run()
+		public static int Part1()
 		{
 			string[] lines = File.ReadAllLines("C:\\Users\\Michael\\AdventOfCode\\2024\\Day1.txt");
 			int[] leftList = new int[lines.Length];
@@ -27,6 +27,31 @@ namespace _2024
 			for (int i = 0; i < leftList.Length; i++)
 			{
 				total += Math.Abs(leftList[i] - rightList[i]);
+			}
+
+			return (total);
+		}
+
+
+		public static int Part2()
+		{
+			string[] lines = File.ReadAllLines("C:\\Users\\Michael\\AdventOfCode\\2024\\Day1.txt");
+			int[] leftList = new int[lines.Length];
+			int[] rightList = new int[lines.Length];
+
+			for (int i = 0; i < lines.Length; i++)
+			{
+				string[] parts = lines[i].Split("   ");
+				leftList[i] = int.Parse(parts[0]);
+				rightList[i] = int.Parse(parts[1]);
+			}
+
+			Array.Sort(leftList);
+			Array.Sort(rightList);
+			int total = 0;
+			for (int i = 0; i < leftList.Length; i++)
+			{
+				total += leftList[i] * rightList.Count(r => r == leftList[i]);
 			}
 
 			return (total);
--- 2024/Program.cs
+++ 2024/Program.cs
@@ -4,5 +4,6 @@ Console.WriteLine("Hello, World!");
 
 
 
-Console.WriteLine(Day1.Run());
+Console.WriteLine(Day1.Part1());
+Console.WriteLine(Day1.Part2());
 Console.ReadKey();
